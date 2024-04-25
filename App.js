0b2004c0c2ba148808f5c864a25b72217fd162a6
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-26 08:51:17 +0900

    3.2 TextInput

--- App.js
+++ App.js
@@ -1,36 +1,69 @@
 import { StatusBar } from "react-native";
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, Pressable } from "react-native";
+import React, { useState } from "react";
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TouchableOpacity, 
+  TouchableHighlight, 
+  TouchableWithoutFeedback, 
+  Pressable,
+  TextInput,
+} from "react-native";
 import { theme } from "./colors";
 
 export default function App(){
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity >
-          <Text style={styles.btnText}>Work</Text> 
+        <TouchableOpacity onPress={work}>
+          <Text 
+            style={{...styles.btnText, color: working ? "white" : theme.gray}}
+          >
+            Work
+          </Text> 
         </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text 
+            style={{...styles.btnText, color: !working ? "white" : theme.gray}}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          onChangeText={onChangeText}
+          value={text}
+          placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+          style={styles.input}
+        />
+      </View>
+    </View>
+  );
+}
+
+
         {/* <TouchableHighlight 
           underlayColor="red"
           onPress={() => console.log("pressed")}
         >
           <Text style={styles.btnText}>Travel</Text>
         </TouchableHighlight> */}
-        <Pressable        
- 
+        {/* <TouchableWithoutFeedback        
           underlayColor="red"
           onPress={() => console.log("pressed")}
         >
           <Text style={styles.btnText}>Travel</Text>
-        </Pressable>
+        </TouchableWithoutFeedback> */}
 
-      </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
     flex: 1,
     backgroundColor: theme.bg,
@@ -46,7 +79,14 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 38 ,
     fontWeight: "600",
-    // color: theme.gray, // 비활성 버튼색
-    color: "white",
+  },
+
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   }
 })
