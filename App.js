515d1d39cc37ef7b412b5b08cef6bd18a959ccf5
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-30 15:49:58 +0900

    3.1 TouchableOpacity : 해당 text를 누르면 클릭되어 text색이 연해졌다가 원색으로 돌아오는 애니메이션

--- App.js
+++ App.js
@@ -1,192 +1,44 @@
-import { Alert, StatusBar } from "react-native";
-import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { 
   StyleSheet, 
   Text, 
-  View, 
-  TouchableOpacity, 
-  TouchableHighlight, 
-  TouchableWithoutFeedback, 
-  Pressable,
-  TextInput,
-  ScrollView,
-  Platform
+  View,
+  TouchableOpacity,
 } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Fontisto } from "@expo/vector-icons";
 import { theme } from "./colors";
 
-const STORAGE_KEY = "@toDos";
-
-export default function App(){
-  const [working, setWorking] = useState(true);
-  const [text, setText] = useState("");
-  const [toDos, setToDos] = useState({});
-  useEffect(() => { 
-    loadToDos();
-  }, []);
-  const travel = () => setWorking(false);
-  const work = () => setWorking(true);
-  const onChangeText = (payload) => setText(payload);
-  const saveToDos = async (toSave) => {
-    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave) )
-  }
-  const loadToDos = async () => {
-   const s = await AsyncStorage.getItem(STORAGE_KEY);
-  //  console.log(s);
-  // console.log(s, JSON.parse(s));
-  if(s){
-
-    setToDos(JSON.parse(s));
-  }
-
-  }
-  
-  const addToDo = async () => {
-    if(text === ""){
-      return
-    }
-    // save to do
-    const newToDos = {
-      ...toDos,
-      [Date.now()] : {text, working},
-    };
-    setToDos(newToDos);
-    await saveToDos(newToDos);
-    setText("");
-  }
-  // console.log(toDos);
-  const deleteToDo = async (key) => {
-    if(Platform.OS === "web"){
-      const ok = confirm("Do you want to delete this To Do?")
-      if(ok) {
-        const newToDos = {...toDos}
-            delete newToDos[key] 
-            setToDos(newToDos);
-            saveToDos(newToDos);
-      }
-    } else{
-      Alert.alert(
-        "Delete To Do?", 
-        "Are you sure?", [
-          {text: "Cancel"},
-          {text: "I'm sure", onPress: () => {
-            const newToDos = {...toDos}
-            delete newToDos[key] 
-            setToDos(newToDos);
-            saveToDos(newToDos);
-          }
-        },
-      ]);
-    }
-    return;
-  }
+export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <View style={styles.header}>
-        <TouchableOpacity onPress={work}>
-          <Text 
-            style={{
-              ...styles.btnText,
-              color: working ? "white" : theme.gray
-            }}
-          >
-            Work
-          </Text> 
+      <View style={styles.header}> 
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={travel}>
-          <Text 
-            style={{ 
-              ...styles.btnText,
-              color: !working ? "white" : theme.gray
-            }}
-          >
-            Travel
-          </Text>
-        </TouchableOpacity>
+          <Text style={styles.btnText}>Travel</Text>
       </View>
-      <View>
-        <TextInput
-          onSubmitEditing={addToDo}
-          onChangeText={onChangeText}
-          returnKeyType="done"
-          value={text}
-          placeholder={working ? "Add a To Do" : "Where do you want to go?"}
-          style={styles.input}
-        />
-      </View>
-      <ScrollView>{
-        Object.keys(toDos).map((key) => (
-          toDos[key].working === working ? (
-            <View style={styles.toDo} key={key}>
-              <Text style={styles.toDoText}>{toDos[key].text}</Text>
-              <TouchableOpacity onPress={() => deleteToDo(key)}>
-                <Fontisto name="trash" size={18} color="white"/>
-              </TouchableOpacity>
-            </View> 
-          ) : null
-        ))}
-      </ScrollView>
     </View>
   );
 }
 
-
-        {/* <TouchableHighlight 
-          underlayColor="red"
-          onPress={() => console.log("pressed")}
-        >
-          <Text style={styles.btnText}>Travel</Text>
-        </TouchableHighlight> */}
-        {/* <TouchableWithoutFeedback        
-          underlayColor="red"
-          onPress={() => console.log("pressed")}
-        >
-          <Text style={styles.btnText}>Travel</Text>
-        </TouchableWithoutFeedback> */}
-
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.bg,
     paddingHorizontal: 20,
+
   },
 
   header: {
     justifyContent: "space-between",
     flexDirection: "row",
     marginTop: 100,
-  },  
+  },
 
-  btnText: {
+  btnText:{
     fontSize: 38,
     fontWeight: "600",
-  },
-
-  input: {
-    backgroundColor: "white",
-    paddingVertical: 15,
-    paddingHorizontal: 20,
-    borderRadius: 30,
-    marginVertical: 20,
-    fontSize: 18,
-  },
-
-  toDo: {
-    backgroundColor: theme.gray,
-    marginBottom: 10,
-    paddingVertical: 20,
-    paddingHorizontal: 40,
-    borderRadius: 15,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-
-  toDoText: {
     color: "white",
-    fontSize: 16,
-    fontWeight: "500",
-  },
-})
+  },  
+});
